a865eef9272dc3e7cc2a36ad52c190f0a74714eb
Roshan <roshanmauryachs@gmail.com>
2025-02-16 15:33:30 +0530

    handle token setting error2

--- api/utils/tocken-cookie-setter.js
+++ api/utils/tocken-cookie-setter.js
@@ -9,7 +9,7 @@ const generateTokenAndSetCookie = (userId, email, res) => {
     maxAge: 15 * 24 * 60 * 60 * 1000,
     httpOnly: true, // prevent XSS attacks cross-site scripting attacks
     sameSite: "strict", // CSRF attacks cross-site request forgery attacks
-    secure: process.env.NODE_ENV === "production",
+    secure: true,
   });
   //console.log("generated-Token-And-Set-Cookie : ", token);
 };
